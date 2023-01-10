9b6b93d5fa3e01095ed0cee51e7cba519e95cbdf
implodingduck <zsek320@gmail.com>
2023-01-10 10:03:22 -0600

    adjusting environment variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -112,7 +112,6 @@ resource "azurerm_storage_account" "sa" {
   location                      = azurerm_resource_group.rg.location
   account_tier                  = "Standard"
   account_replication_type      = "LRS"
-  public_network_access_enabled = false
 }
 
 resource "azurerm_service_plan" "asp" {
