3f8e300341389fca1f430737248b09618c90d36f
implodingduck <zsek320@gmail.com>
2023-01-10 12:38:35 -0600

    adding eh producer to timer

--- func/TimerTrigger/index.js
+++ func/TimerTrigger/index.js
@@ -1,9 +1,12 @@
 const { delay, ServiceBusClient } = require("@azure/service-bus");
+const { EventHubProducerClient } = require("@azure/event-hubs");
 const { DefaultAzureCredential } = require("@azure/identity");
 
 const fullyQualifiedNamespace = process.env.SERVICE_BUS_NAMESPACE__fullyQualifiedNamespace;
+const ehFullyQualifiedNamespace = EVENT_HUB_NAMESPACE__fullyQualifiedNamespace;
 const credential = new DefaultAzureCredential();
 const topicName = process.env.TOPIC_NAME;
+const eventHubName = "funceventhub";
 
 module.exports = async function (context, myTimer) {
     var timeStamp = new Date().toISOString();
@@ -14,11 +17,14 @@ module.exports = async function (context, myTimer) {
     }
     context.log('JavaScript timer trigger function ran!', timeStamp);   
     const sbClient = new ServiceBusClient(fullyQualifiedNamespace, credential);
+    const producer = new EventHubProducerClient(fullyQualifiedNamespace, eventHubName, credential);
 
     const receiver = sbClient.createReceiver(topicName, "subfuncservicebustopic");
 
+    const batch = await producer.createBatch();
     const myMessageHandler = async (messageReceived) => {
 		context.log(`Received message: ${messageReceived.body}`);
+        batch.tryAdd({ body: messageReceived.body });
     };
 
     const myErrorHandler = async (error) => {
@@ -32,8 +38,12 @@ module.exports = async function (context, myTimer) {
 
 	// Waiting long enough before closing the sender to send messages
 	await delay(5000);
-
+    if (batch.count() > 0){
+        context.log('Sending Batch...');
+        await producer.sendBatch(batch);
+    }
+    
 	await receiver.close();	
 	await sbClient.close();
-
+    await producer.close();
 };
